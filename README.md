03f5d07ce639742c1988eccc6535d5a96a35e292
Justin Kuspa <kuspaj@kuspaj-a01.vmware.com>
2022-06-13 11:04:18 -0400

    adding initial chart config

---
+++ README.md
@@ -0,0 +1,6 @@
+# This is a test Jira installation that shows how to configure certain flags. 
+
+
+## Requests
+
+https://atlassian.github.io/data-center-helm-charts/userguide/resource_management/REQUESTS_AND_LIMITS/
---
+++ install.sh
@@ -0,0 +1,31 @@
+# This is a test Jira installation that shows how to configure certain flags. 
+
+## Add the Atlassian helm repo
+# helm repo add atlassian-data-center \
+#  Docs: https://atlassian.github.io/data-center-helm-charts
+#  Code: https://github.com/atlassian/data-center-helm-charts/tree/main/src/main/charts
+
+# Update the repo 
+# helm repo update
+
+# Show all of the charts
+# helm search repo atlassian-data-center
+
+# VERSION
+# VERSION="1.4.1"
+
+# Download the version set above
+# helm pull atlassian-data-center/jira --version $VERSION
+
+# Use helm upgrade, so you can upgrade from version to version
+helm --debug upgrade --install jira ./jira-$VERSION.tgz \
+--set image.tag=8.20.9 \
+--set jira.clustering.enabled=true \
+--set requests.cpu=2 \
+--set requests.memory=2G \
+--create-namespace \
+--namespace jira
+
+
+# Command to delete, do not uncomment
+# helm delete jira -n jira
